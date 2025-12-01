5f4ac0e2e9280ad25a6da676e37d974cfd40c91a
CholponDev <januzakova.cholpon@gmail.com>
2025-12-01 15:50:39 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let kidChopa = {
+    name: "Cholpon",
+    ege: 7,
+    class: "1-class",
+    hobby: "basketbool"
+}
+
+console.log(kidChopa)
+
+kidChopa.ege = 23
+kidChopa.class = "student"
+kidChopa.hobby = "learn languages"
+growth = 1.63,
+favoriteSubject = "english"
